604bdbb3e77630700911c62abb5d3c3d2d64ab78
termisanju <psja2006@gmail.com>
2025-12-04 12:49:27 +0100

    company App

--- src/main/java/org/termi/CompanyApp.java
+++ src/main/java/org/termi/CompanyApp.java
@@ -15,19 +15,22 @@ public class CompanyApp {
         ReaderDepartamento readerDepartamento = new ReaderDepartamento(scanner,readerEmpleado);
         ReaderEmpresa readerEmpresa = new ReaderEmpresa(scanner,readerDepartamento);
         Empresa empresa = readerEmpresa.read();
-        do {
-            menu();
-            int a = scanner.nextInt();
-            scanner.nextLine();
 
-        } while ();
+        menu();
+        int a = scanner.nextInt();
+        scanner.nextLine();
+        do {
+
+        } while (a != 4);
 
     }
 
 
     public static void menu(){
+        System.out.println("Dime un número del 1 al 4");
         System.out.println("1. Nombre de departamento = mostrar sus datos");
         System.out.println("2. Nombre de departamento = mostrar sus empleados");
         System.out.println("3. Nombre de departamento + nif = mostrar datos del empleado del departamento indicado");
+        System.out.println("4. Adios");
     }
 }
