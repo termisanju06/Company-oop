44bb344763836451d6c29ac30a1a163425312cc3
termisanju <psja2006@gmail.com>
2025-12-04 11:02:48 +0100

    Company objetos model y readers

--- src/main/java/org/termi/model/Departamento.java
+++ src/main/java/org/termi/model/Departamento.java
@@ -1,5 +1,62 @@
 package org.termi.model;
 
-public class Departamento {
+import java.util.Arrays;
+import java.util.Objects;
 
+public class Departamento {
+    private String name;
+    private String presupuesto;
+    private Empleado[] Empleado;
+
+    public Departamento(String name, String presupuesto, Empleado[] empleado) {
+        this.name = name;
+        this.presupuesto = presupuesto;
+        Empleado = empleado;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPresupuesto() {
+        return presupuesto;
+    }
+
+    public void setPresupuesto(String presupuesto) {
+        this.presupuesto = presupuesto;
+    }
+
+    public Empleado[] getEmpleado() {
+        return Empleado;
+    }
+
+    public void setEmpleado(Empleado[] empleado) {
+        Empleado = empleado;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Departamento that = (Departamento) o;
+        return Objects.equals(name, that.name) && Objects.equals(presupuesto, that.presupuesto) && Objects.deepEquals(Empleado, that.Empleado);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, presupuesto, Arrays.hashCode(Empleado));
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento{" +
+                "name='" + name + '\'' +
+                ", presupuesto='" + presupuesto + '\'' +
+                ", Empleado=" + Arrays.toString(Empleado) +
+                '}';
+    }
 }
+
