69a25d3501ab96d0ebc27d2e0751892e7addbd69
termisanju <psja2006@gmail.com>
2025-12-04 11:47:07 +0100

    reader empleado

--- src/main/java/org/termi/readers/ReaderEmpleado.java
+++ src/main/java/org/termi/readers/ReaderEmpleado.java
@@ -1,4 +1,32 @@
 package org.termi.readers;
 
+import org.termi.model.Empleado;
+
+import java.util.Scanner;
+
 public class ReaderEmpleado {
+    private final Scanner sc;
+    private final ReaderEmpleado readerEmpleado;
+
+    public ReaderEmpleado(Scanner sc, ReaderEmpleado readerEmpleado) {
+        this.sc = sc;
+        this.readerEmpleado = readerEmpleado;
+    }
+
+    public Empleado read() {
+        System.out.println("Dame tu nombre empleado");
+        String name = sc.nextLine();
+
+        System.out.println("Dame tu apellido empleado");
+        String surname = sc.nextLine();
+
+        System.out.println("Dime tu nif empleado");
+        String nif = sc.nextLine();
+
+        System.out.println("Dime tu puesto empleado");
+        String puesto = sc.nextLine();
+
+
+        return new Empleado(name, surname, nif, puesto);
+    }
 }
