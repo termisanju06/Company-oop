3dabeaa300ac2d83f97e5abb478c20e07a99fb7b
termisanju <psja2006@gmail.com>
2025-12-04 12:19:53 +0100

    reader departamento

--- src/main/java/org/termi/readers/ReaderEmpresa.java
+++ src/main/java/org/termi/readers/ReaderEmpresa.java
@@ -1,8 +1,34 @@
 package org.termi.readers;
 
+import org.termi.model.Departamento;
+import org.termi.model.Empresa;
+
 import java.util.Scanner;
 
 public class ReaderEmpresa {
-    private final Scanner sc;
+    private final Scanner scanner;
+    private final ReaderDepartamento readerDepartamento;
 
+    public ReaderEmpresa(Scanner scanner, ReaderDepartamento readerDepartamento) {
+        this.scanner = scanner;
+        this.readerDepartamento = readerDepartamento;
+    }
+    public Empresa read(){
+        System.out.println("Dame tu nombre de empresa");
+        String name = scanner.nextLine();
+
+        System.out.println("Dame tu cif de empresa");
+        String cif = scanner.nextLine();
+
+        System.out.println("Cuantos departamentos tiene la empresa");
+        int a = scanner.nextInt();
+        scanner.nextLine();
+        Departamento[] departamentos = new Departamento[a];
+
+        for (int i = 0; i < a; i++) {
+            departamentos[i] = readerDepartamento.read();
+        }
+
+        return new Empresa(name,cif,departamentos);
+    }
 }
